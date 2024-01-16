09c4b3310a1d980406c9dd226c0917e24894c074
JaimeAdele <jaime.zuspann@gmail.com>
2024-01-15 20:06:42 -0800

    add pagination and search capabilities

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
 import { useState, useEffect } from 'react';
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import PostDetails from './components/PostDetails/PostDetails';
 import Auth from './components/Auth/Auth';
 
-const App = () => (
-  <GoogleOAuthProvider clientId='492237408024-db0ebg532k885kgp8qldgupgc1g6a3sm.apps.googleusercontent.com'>
-    <BrowserRouter>
-      <Container maxwidth='lg'>
-        <Navbar />
-        <Routes>
-          <Route path='/' exact element={<Home />} />
-          <Route path='auth' element={<Auth />} />
-        </Routes>
-      </Container>
-    </BrowserRouter>
-  </GoogleOAuthProvider>
-);
+const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'));
+
+  return (
+    <GoogleOAuthProvider clientId='492237408024-db0ebg532k885kgp8qldgupgc1g6a3sm.apps.googleusercontent.com'>
+      <BrowserRouter>
+        <Container maxwidth='xl'>
+          <Navbar />
+          <Routes>
+            <Route path='/' exact element={<Navigate to='/posts' />} />
+            <Route path='/posts' exact element={<Home />} />
+            <Route path='/posts/search' exact element={<Home />} />
+            <Route path='/posts/:id' element={<PostDetails />} />
+            {/* only go to auth page if user is not logged in */}
+            <Route path='auth' element={user ? <Navigate to='/posts' /> : <Auth />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  );
+}
 
 export default App;
